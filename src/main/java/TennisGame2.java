3a92703f0efa50f581f86524e34c439a9e7d02ef
Kanawat Phuengphadung <kanawatphu@mathregit.group>
2020-08-04 11:07:22 +0700

    delete uncoverage method.

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -99,24 +99,6 @@ public class TennisGame2
         return score;
     }
 
-    public void SetP1Score(int number){
-
-        for (int i = 0; i < number; i++)
-        {
-            P1Score();
-        }
-
-    }
-
-    public void SetP2Score(int number){
-
-        for (int i = 0; i < number; i++)
-        {
-            P2Score();
-        }
-
-    }
-
     public void P1Score(){
         P1point++;
     }
