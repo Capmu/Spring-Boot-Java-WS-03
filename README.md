e748bba341f516162aa8771e75752f37a0d3046a
Kanawat Phuengphadung <kanawatphu@mathregit.group>
2020-08-04 23:58:54 +0700

    refact Equal and ducing case

--- README.md
+++ README.md
@@ -6,4 +6,6 @@
 2. replace ```p2Point``` to ```player2Point```
 3. replace ```p1Res``` to ```player1Result```
 4. replace ```p2Res``` to ```player2Result```
-5. duplicate condition ```player1Point == player2Point``` 
+5. duplicate condition ```player1Point == player2Point```
+5. duplicate condition ```player1Point > player2Point```  
+5. duplicate condition ```player2Point > player1Point``` 
--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -12,21 +12,21 @@ public class TennisGame2
     }
 
     public String getScore(){
+        String[] scoreWords = {"Love", "Fifteen", "Thirty", "Forty"};
         String player1Result = "";
         String player2Result = "";
         String score = "";
-        if (player1Point == player2Point) {
-            if (player1Point < 4) {
-                if (player1Point == 0)
-                    score = "Love";
-                if (player1Point == 1)
-                    score = "Fifteen";
-                if (player1Point == 2)
-                    score = "Thirty";
-                score += "-All";
-            }
-            if (player1Point >= 3)
-                score = "Deuce";
+
+        //Score is equal, but less than 4
+        boolean isEqualNotDeuce = (player1Point == player2Point && player1Point < 3);
+        if(isEqualNotDeuce) {
+            return scoreWords[player1Point] + "-All";
+        }
+
+        //Deucing case
+        boolean isDeuce = (player1Point == player2Point && player1Point >= 3);
+        if(isDeuce){
+            return "Deuce";
         }
 
         if (player1Point > 0 && player2Point ==0)
@@ -41,6 +41,7 @@ public class TennisGame2
             player2Result = "Love";
             score = player1Result + "-" + player2Result;
         }
+
         if (player2Point > 0 && player1Point ==0)
         {
             if (player2Point ==1)
@@ -54,39 +55,38 @@ public class TennisGame2
             score = player1Result + "-" + player2Result;
         }
 
-        if (player1Point > player2Point && player1Point < 4)
-        {
-            if (player1Point ==2)
-                player1Result="Thirty";
-            if (player1Point ==3)
-                player1Result="Forty";
-            if (player2Point ==1)
-                player2Result="Fifteen";
-            if (player2Point ==2)
-                player2Result="Thirty";
-            score = player1Result + "-" + player2Result;
-        }
-        if (player2Point > player1Point && player2Point < 4)
-        {
-            if (player2Point ==2)
-                player2Result="Thirty";
-            if (player2Point ==3)
-                player2Result="Forty";
-            if (player1Point ==1)
-                player1Result="Fifteen";
-            if (player1Point ==2)
-                player1Result="Thirty";
-            score = player1Result + "-" + player2Result;
+        if (player1Point > player2Point){
+            if (player1Point < 4) {
+                if (player1Point == 2)
+                    player1Result = "Thirty";
+                if (player1Point == 3)
+                    player1Result = "Forty";
+                if (player2Point == 1)
+                    player2Result = "Fifteen";
+                if (player2Point == 2)
+                    player2Result = "Thirty";
+                score = player1Result + "-" + player2Result;
+            }
+            else if (player2Point >= 3) {
+                score = "Advantage player1";
+            }
         }
 
-        if (player1Point > player2Point && player2Point >= 3)
-        {
-            score = "Advantage player1";
-        }
-
-        if (player2Point > player1Point && player1Point >= 3)
-        {
-            score = "Advantage player2";
+        if (player2Point > player1Point){
+            if (player2Point < 4) {
+                if (player2Point == 2)
+                    player2Result = "Thirty";
+                if (player2Point == 3)
+                    player2Result = "Forty";
+                if (player1Point == 1)
+                    player1Result = "Fifteen";
+                if (player1Point == 2)
+                    player1Result = "Thirty";
+                score = player1Result + "-" + player2Result;
+            }
+            else if (player1Point >= 3) {
+                score = "Advantage player2";
+            }
         }
 
         if (player1Point >=4 && player2Point >=0 && (player1Point - player2Point)>=2)
